1ed8f3128469e17420cbc65aae129bf3ef5eefe5
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2020-11-04 22:20:09 -0800

    Original

--- CodePractice/CodePractice/ArraysPrograms.cs
+++ CodePractice/CodePractice/ArraysPrograms.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace CodePractice
 {
@@ -28,5 +29,68 @@ namespace CodePractice
 
             return sumExpected-sumReal;
         }
+
+        /// <summary>
+        /// Given an unsorted array of nonnegative integers, find a continuous subarray which adds to a given number.
+        /// </summary>
+        /// <param name="array">The positive integers array</param>
+        /// <param name="sum">the number the subarray must add to</param>
+        /// <returns>An array with the indexes where the subarray is located, null if nothing was found</returns>
+        static public uint [] SubArraySum(uint[] array, uint sum)
+        {
+            uint[] indexes = null;
+
+            if (array == null) return null;
+
+            indexes = new uint[2];
+            if(array.Length==1)
+            {
+                if (array[0] == sum)
+                {
+                   
+                    return indexes;
+                } 
+                else
+                {
+                    return null;
+                }
+            }
+
+            uint subSum = array[0];
+            bool nonSumFound = true;
+            bool addedNumber = false;
+            for (uint i = 1; i < array.Length; i++)
+            {
+                if (subSum < sum) //hay que seguir sumando
+                {
+                    subSum += array[i];
+                    indexes[1]++;
+                    addedNumber = true;
+                }
+                else if (subSum > sum) // me pase de la suma
+                {
+                    indexes[0] = indexes[1];  //reinicio los indices del subarreglo
+                    indexes[1] = indexes[1];
+                    if (addedNumber)
+                    {
+                        i--;
+                        addedNumber=false; //reinicio la bandera de suma
+                    }
+                    subSum = array[i];
+                }
+                else //son iguales!! la encontre
+                {
+                    nonSumFound = false;
+                    break;
+                }
+
+            }
+
+            if (nonSumFound) return null;
+
+            return indexes;
+        }
+
+        //Comparar dos cadenas. La primera tiene numeros y letras. La segunda es un patron donde # representa 1 digito y *una letra o numero pero no espacios
     }
 }
--- CodePractice/CodePractice/Program.cs
+++ CodePractice/CodePractice/Program.cs
@@ -11,6 +11,16 @@ namespace CodePractice
             uint[] a = { 1, 2, 3, 4, 5, 6, 8, 9, 10 };
 
             Console.WriteLine("Missing integer is: {0}", ArraysPrograms.MissingInteger(a));
+
+            Console.WriteLine("Encontrando las suma");
+
+            uint[] b = { 4,5,10,8,1,7};
+            uint sum = 6;
+            uint[] indexes = ArraysPrograms.SubArraySum(b, sum);
+            if (indexes == null)
+                Console.WriteLine("no hay subarreglo que sume {0}", sum);
+            else
+                Console.WriteLine("el subarray que suma {2} esta entre los {0}  y  {1} indices", indexes[0], indexes[1], sum);
         }
     }
 }
